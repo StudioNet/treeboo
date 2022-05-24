0bb944dbc58c8e6804339d9d362480a75a237f51
Evgeny Yurovitsky <evgeny.yurovitsky@dealhub.io>
2022-05-24 23:07:57 +0300

    fix name and prefix

--- projects/treepecan/src/app/app.component.spec.ts
+++ projects/treepecan/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'treebob'`, () => {
+  it(`should have as title 'treepecan'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('treebob');
+    expect(app.title).toEqual('treepecan');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('treebob app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('treepecan app is running!');
   });
 });
--- projects/treepecan/src/app/app.component.ts
+++ projects/treepecan/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'bob-root',
+  selector: 'pcn-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'treebob';
+  title = 'treepecan';
 }
